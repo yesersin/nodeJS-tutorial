596728e058f639ea4c23e84078ee76ef34a211a2
yesersin <yesersin@gmail.com>
2021-08-30 23:15:16 +0300

    kalkanlar devrede mi ?

--- d4-body-parser/index.js
+++ d4-body-parser/index.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+
 var app = express();
+app.use(express.static('public')); //artık herhangi bir yerde public/style.css dememe gerek yok. style.css diye cagırırım.
 app.use(bodyParser.urlencoded({extended:true})); //standart kullanımmış!
 
 var sehirler = ["istanbul","hatay","tokat","gaziantep"];
@@ -9,6 +11,23 @@ var sehirler = ["istanbul","hatay","tokat","gaziantep"];
 app.get("/sehirler", function(i,c) {
     c.send(sehirler);
     
+});
+
+//?: zorunlu olmayan
+//* :herhangi bir deger
+//+ sonra gelen herhangi bir şey.
+app.get("/sehir?",function(i,c){
+    console.log(i);
+});
+
+//get,post,put,delete ne varsa buraya yönlendir demek.
+app.all("/sehir",function(i,c){
+    console.log(i);
+});
+
+//burda ? işareti ile olsada olur olmasada dedik.
+app.all("/sehir/:id?",function(i,c){
+    console.log(i);
 });
 
 app.post("/sehirlerEkle", function(i,c) {
